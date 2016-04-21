006d32796f81f2c096ae236442ad159eb0a1b3ac
Syntaxious <SynPlaysMC@gmail.com>
2016-04-21 17:50:35 +0100

    Update Main.php

--- src/BackToSpawn/Main.php
+++ src/BackToSpawn/Main.php
@@ -10,18 +10,19 @@ use pocketmine\utils\TextFormat;
 class SpawnPort extends PluginBase{
 
     public function onEnable(){
+        $this->getLogger()->info("[BackToSpawn] enabled")
     }
     public function onDisable(){
     }
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        if(strtolower($command->getName()) === "spawnport"){
+        if(strtolower($command->getName()) === "s"){
             if(!($sender instanceof Player)){
-                $sender->sendMessage(TextFormat::AQUA."Please run this command in game");
+                $sender->sendMessage(TextFormat::RED."Please run this command in game");
                 return true;
             }
             $worldspawn = $sender->getLevel()->getSpawnLocation();
             $sender->teleport($worldspawn);
-            $sender->sendMessage(TextFormat::AQUA."Teleported to WorldSpawn");
+            $sender->sendMessage(TextFormat::GREEN."Teleported to Spawn");
         }
         return true;
     }
