09518ddae945ee63b17d952f4340c67f6d1f2567
Syntaxious <SynPlaysMC@gmail.com>
2016-04-21 17:55:02 +0100

    Update Main.php

--- src/BackToSpawn/Main.php
+++ src/BackToSpawn/Main.php
@@ -13,14 +13,18 @@ class SpawnPort extends PluginBase{
     public function onEnable(){
         $this->getLogger()->info("[BackToSpawn] enabled")
     }
+    
     public function onDisable(){
+        $this->getLogger()->info("[BackToSpawn] enabled")
     }
+    
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "s"){
             if(!($sender instanceof Player)){
                 $sender->sendMessage(TextFormat::RED."Please run this command in game");
                 return true;
             }
+            
             $worldspawn = $sender->getLevel()->getSpawnLocation();
             $sender->teleport($worldspawn);
             $sender->sendMessage(TextFormat::GREEN."Teleported to Spawn");
