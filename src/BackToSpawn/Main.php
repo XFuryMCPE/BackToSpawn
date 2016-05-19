1b191e073af1985793df7d2edf3e9126d6c3c586
XFuryMCPE <xfurymcpe@gmail.com>
2016-05-19 19:41:00 -0400

    Rewrote because I felt like it

--- src/BackToSpawn/Main.php
+++ src/BackToSpawn/Main.php
@@ -8,27 +8,35 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class BackToSpawn extends PluginBase{
+class Main extends PluginBase{
 
     public function onEnable(){
         $this->getLogger()->info("[BackToSpawn] enabled")
     }
     
-    public function onDisable(){
-        $this->getLogger()->info("[BackToSpawn] enabled")
-    }
-    
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "spawn"){
-            if(!($sender instanceof Player)){
-                $sender->sendMessage(TextFormat::RED."Please run this command in game");
-                return true;
+            if(!$sender instanceof Player){
+                $sender->sendMessage("Please run this command in-game");
+                return;
             }
-            
-            $worldspawn = $sender->getLevel()->getSpawnLocation();
-            $sender->teleport($worldspawn);
-            $sender->sendMessage(TextFormat::GREEN."Teleported to Spawn");
+            if(count($args) > 1 or count($args) < 1){
+                $sender->sendMessage(TextFormat::RED."Usage: /spawn <default:here>");
+                return;
+            }
+            switch(strtolower($args[0])){
+                case "default":
+                    $sender->teleport($this->getServer()->getDefaultLevel()->getSpawnLocation());
+                    $sender->sendMessage(TextFormat::GREEN."Teleported to the server's default spawn!");
+                break;
+                case "here":
+                    $sender->teleport($sender->getLevel()->getSpawnLocation());
+                    $sender->sendMessage(TextFormat::GREEN."Teleported to this world's spawn!");
+                break;
+                default:
+                   $sender->sendMessage(TextFormat::RED."Usage: /spawn <default:here>");
+                break;
+            }
         }
-        return true;
     }
 }
